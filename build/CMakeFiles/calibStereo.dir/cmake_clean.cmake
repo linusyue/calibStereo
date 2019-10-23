cb39025050bc8722ed1db7ed26acb2dd7516de13
linusyue <93686722@qq.com>
2019-10-23 09:25:21 +0800

    initial code

---
+++ build/CMakeFiles/calibStereo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibStereo.dir/stereo_calib.cpp.o"
+  "calibStereo.pdb"
+  "calibStereo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibStereo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
